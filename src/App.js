6a182dcd7b2bd98a35d459b494cdc1b06bc88a25
patrickmurray73 <g00344530@gmit.ie>
2020-12-21 21:15:08 +0000

    Added some basic Routing

--- src/App.js
+++ src/App.js
@@ -3,20 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Nav} from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 class App extends Component {
   render(){
   return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">CryptoLog</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-      </Navbar>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">CryptoLog</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/read">Read</Nav.Link>
+          <Nav.Link href="/create">Create</Nav.Link>
+        </Nav>
+        </Navbar>
+        <Switch>
+          <Route path='/' component={content} exact/>
+          <Route path='/create' component={content} exact/>
+          <Route path='/read' component={content} exact/>
+        </Switch>
+      </div>
+    </Router>
   );
   }
 }
