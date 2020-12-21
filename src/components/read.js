9a50b5c8f564aed2b24ee70d3e7fe989736b25ac
patrickmurray73 <g00344530@gmit.ie>
2020-12-21 21:36:02 +0000

    Added temporary portfolio

--- src/components/read.js
+++ src/components/read.js
@@ -2,6 +2,29 @@ import React from 'react';
 
 export class Read extends React.Component {
 
+    state = {
+        portfolio: [
+            {
+                "Logo": "https://cryptologos.cc/logos/bitcoin-btc-logo.png?v=008",
+                "Ticker": "Btc",
+                "Price": "23000",
+                "Holdings": "14.365"
+            },
+            {
+                "Logo": "https://cryptologos.cc/logos/ethereum-eth-logo.png?v=008",
+                "Ticker": "Eth",
+                "Price": "550",
+                "Holdings": "184.6"
+            },
+            {
+                "Logo": "https://cryptologos.cc/logos/ocean-protocol-ocean-logo.png?v=008",
+                "Ticker": "Ocean",
+                "Price": "0.43",
+                "Holdings": "225036.3"
+            }
+        ]
+    };
+
     render() {
         return(
             <div>
