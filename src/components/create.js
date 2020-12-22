e0a44d91d943a8da1871fa9821748fb4df66155b
patrickmurray73 <g00344530@gmit.ie>
2020-12-22 16:41:21 +0000

    Added onSubmit an Add Logo form

--- src/components/create.js
+++ src/components/create.js
@@ -4,20 +4,31 @@ export class Create extends React.Component {
 
     constructor() {
         this.super();
-        
+
         this.state = {
             Logo: '',
             Ticker: '',
             Price: '',
             Holdings: ''
         }
+    }
 
+    onSubmit(e) {
+        e.preventDefault();
     }
 
     render() {
         return(
-            <div>
-                <h1>This is the create component</h1>
+            <div className='App'>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Add Logo: </label>
+                        <input type='text'
+                        className='form-control'
+                        value={this.state.Logo}
+                        onChange={this.onChangeLogo}></input>
+                    </div>
+                </form>
             </div>
         )
     }
