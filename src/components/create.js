da2383ce7afea9d99ccf2671469fd1549710d49b
patrickmurray73 <g00344530@gmit.ie>
2020-12-22 17:35:33 +0000

    Finished Create form and added relevant functions

--- src/components/create.js
+++ src/components/create.js
@@ -6,8 +6,11 @@ export class Create extends React.Component {
         super();
 
         this.onSubmit = this.onSubmit.bind(this);
-        this.onChangeLogo = this.onChangeTitle.bind(this);
-        
+        this.onChangeTicker = this.onChangeTicker.bind(this);
+        this.onChangePrice = this.onChangePrice.bind(this);
+        this.onChangeHoldings = this.onChangeHoldings.bind(this);
+        this.onChangeLogo = this.onChangeLogo.bind(this);
+
         this.state = {
             Logo: '',
             Ticker: '',
@@ -16,14 +19,33 @@ export class Create extends React.Component {
         }
     }
 
+    onChangeTicker(e) {
+        this.setState({
+            Ticker: e.target.value
+        });
+    }
+
+    onChangePrice(e) {
+        this.setState({
+            Price: e.target.value
+        });
+    }
+
+    onChangeHoldings(e) {
+        this.setState({
+            Holdings: e.target.value
+        });
+    }
+
     onChangeLogo(e) {
         this.setState({
-            Logo: e.Target.value
+            Logo: e.target.value
         });
     }
 
     onSubmit(e) {
         e.preventDefault();
+        alert(this.state.Ticker + ' added');
     }
 
     render() {
@@ -31,11 +53,37 @@ export class Create extends React.Component {
             <div className='App'>
                 <form onSubmit={this.onSubmit}>
                     <div className="form-group">
+                        <label>Add Ticker: </label>
+                        <input type='text'
+                        className='form-control'
+                        value={this.state.Ticker}
+                        onChange={this.onChangeTicker}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Add Price: </label>
+                        <input type='text'
+                        className='form-control'
+                        value={this.state.Price}
+                        onChange={this.onChangePrice}></input>
+                    </div>
+                    <div className="form-group">
+                        <label>Add Holdings: </label>
+                        <input type='text'
+                        className='form-control'
+                        value={this.state.Holdings}
+                        onChange={this.onChangeHoldings}></input>
+                    </div>
+                    <div className="form-group">
                         <label>Add Logo: </label>
                         <input type='text'
                         className='form-control'
                         value={this.state.Logo}
                         onChange={this.onChangeLogo}></input>
+                    </div>
+                    <div className="form-group">
+                        <input type='submit'
+                        value='Add Crypto'
+                        className='btn btn-dark'></input>
                     </div>
                 </form>
             </div>
