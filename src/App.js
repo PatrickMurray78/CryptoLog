9b40564b948ef45606267eb075cf0bb706298c66
patrickmurray73 <g00344530@gmit.ie>
2020-12-26 12:31:46 +0000

    Changed read.js to portfolio.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav, Container} from 'react-bootstrap';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import { Content } from './components/content';
-import { Read } from './components/read';
+import { Read } from './components/portfolio';
 import { Create } from './components/create';
 import { Edit } from './components/edit';
 import Link from 'react-router-dom/Link';
@@ -16,12 +14,11 @@ class App extends Component {
     <Router>
       <div className="App">
         <div style={{backgroundColor: "#303030", padding: "5px", fontFamily: "monospace"}}>
-          <Link to='/read'><h1 style={{color: "white"}}>CryptoLog</h1></Link>
+          <Link to='/'><h1 style={{color: "white"}}>CryptoLog</h1></Link>
         </div>
         <Switch>
-          <Route path='/' component={Content} exact/>
+          <Route path='/' component={Read} exact/>
           <Route path='/create' component={Create}/>
-          <Route path='/read' component={Read}/>
           <Route path='/edit/:id' component={Edit}/>
         </Switch>
       </div>
